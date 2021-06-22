88643e694892718412f53b0af0109a73dc6bcbdd
kotu-kotuo <ttn-0223@mineo.jp>
2021-06-22 20:56:59 +0900

    eslintrc修正

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,23 @@
 module.exports = {
-  plugins: ["prettier"],
-  extends: ["plugin:react/recommended", "plugin:tailwind/recommended"],
-  rules: {
-    "prettier/prettier": "error",
+  parserOptions: {
+    sourceType: "module",
+    ecmaVersion: 2015,
   },
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended", // TypeScriptでチェックされる項目をLintから除外する設定
+    "plugin:prettier/recommended",
+  ],
+  plugins: ["@typescript-eslint"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    sourceType: "module",
+    project: "./src/tsconfig.json", // TypeScriptのLint時に参照するconfigファイルを指定
+  },
+  root: true, // 上位ディレクトリにある他のeslintrcを参照しないようにする
+  rules: {},
 };
