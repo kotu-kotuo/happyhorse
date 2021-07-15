cf6eeb35b7b4a6424b78f39a3f3b8609e3bfcf3b
kotu-kotuo <ttn-0223@mineo.jp>
2021-07-15 16:26:14 +0900

    vercelに環境変数を設定

--- src/firebase/admin.ts
+++ src/firebase/admin.ts
@@ -1,27 +1,13 @@
 import * as admin from "firebase-admin";
 
-if (
-  process.env.NODE_ENV === "production" 
-) {
-  if (!admin.apps.length) {
-    admin.initializeApp({
-      credential: admin.credential.cert({
-        projectId: process.env.FIREBASE_PROJECT_ID_PROD,
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL_PROD,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY_PROD?.replace(/\\n/g, "\n"),
-      }),
-    });
-  }
-} else {
-  if (!admin.apps.length) {
-    admin.initializeApp({
-      credential: admin.credential.cert({
-        projectId: process.env.FIREBASE_PROJECT_ID_DEV,
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL_DEV,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY_DEV?.replace(/\\n/g, "\n"),
-      }),
-    });
-  }
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    }),
+  });
 }
 
 export default admin;
