794c689b329c3dfd7fa9396beec5336af37d6724
kotu-kotuo <ttn-0223@mineo.jp>
2021-07-20 12:28:20 +0900

    PostRules.test修正

--- __tests__/PostRules.test.tsx
+++ __tests__/PostRules.test.tsx
@@ -14,113 +14,119 @@ const myId = "user_abc";
 const theirId = "user_xyz";
 const auth = { uid: myId, email: "user_abc@gmail.com" };
 
-describe("posts", () => {
+describe("post", () => {
   // テスト開始時に1回だけ実行される
   beforeAll(async () => {
     // ローカルにあるRulesファイルを読み込む
     await helper.loadRules(projectID);
   });
 
-  describe("get operation", () => {
-    describe("user is not authenticated", () => {
-      let ref;
-      beforeEach(async () => {
-        ref = helper
-          .adminApp(projectID)
-          .firestore()
-          .collection("users")
-          .doc(myId)
-          .collection("posts")
-          .doc();
+  describe("collection posts", () => {
+    describe("get operation", () => {
+      describe("user is authenticated", () => {
+        let ref;
+        beforeEach(async () => {
+          ref = helper
+            .adminApp(projectID)
+            .firestore()
+            .collection("users")
+            .doc(myId)
+            .collection("posts")
+            .doc();
 
-        await ref.set({
-          postID: "postID",
-          userID: myId,
-          username: "username",
-          avatar: "avatar",
-          images: ["image", "image2"],
-          title: "title",
-          postText: "postText",
-          horseName: "horseName",
-          category: "category",
-          breed: "breed",
-          gender: "gender",
-          color: "color",
-          birth: { year: 2000, month: 2, day: 2 },
-          age: 11,
-          height: 160,
-          area: "area",
-          features: ["empty"],
-          price: 3000000,
-          createdAt: adminFirestore.FieldValue.serverTimestamp(),
-          updatedAt: "",
-          likeUserIDs: [theirId],
-          isAvairable: true,
-          pv: 0,
-          sendMessageUserIDs: [theirId],
-          messageUpdatedAt: "",
-          latestMessage: "ooooo",
-          clientUserID: theirId,
-          ratingCompleted: false,
-          deletedAccount: false,
+          await ref.set({
+            postID: "postID",
+            userID: myId,
+            username: "username",
+            avatar: "avatar",
+            images: ["image", "image2"],
+            title: "title",
+            postText: "postText",
+            horseName: "horseName",
+            category: "category",
+            breed: "breed",
+            gender: "gender",
+            color: "color",
+            birth: { year: 2000, month: 2, day: 2 },
+            age: 11,
+            height: 160,
+            area: "area",
+            features: ["empty"],
+            price: 3000000,
+            createdAt: adminFirestore.FieldValue.serverTimestamp(),
+            updatedAt: "",
+            likeUserIDs: [theirId],
+            isAvairable: true,
+            pv: 0,
+            sendMessageUserIDs: [theirId],
+            messageUpdatedAt: "",
+            latestMessage: "ooooo",
+            clientUserID: theirId,
+            ratingCompleted: false,
+            deletedAccount: false,
+          });
         });
-      });
 
-      test("should be failed", () => {
-        const db = helper.app(projectID).firestore();
-        assertFails(db.doc(ref.path).get());
+        test("should be succeeded", () => {
+          const db = helper.app(projectID, auth).firestore();
+          assertSucceeds(db.doc(ref.path).get());
+          assertSucceeds(db.collectionGroup("posts").get());
+        });
       });
     });
 
-    describe("user is authenticated", () => {
-      let ref;
-      beforeEach(async () => {
-        ref = helper
-          .adminApp(projectID)
-          .firestore()
-          .collection("users")
-          .doc(myId)
-          .collection("posts")
-          .doc();
+    describe("create update operation", () => {
+      describe("user authenticated", () => {
+        test("should be succeeded", () => {
+          const db = helper.app(projectID, auth).firestore();
 
-        await ref.set({
-          postID: "postID",
-          userID: myId,
-          username: "username",
-          avatar: "avatar",
-          images: ["image", "image2"],
-          title: "title",
-          postText: "postText",
-          horseName: "horseName",
-          category: "category",
-          breed: "breed",
-          gender: "gender",
-          color: "color",
-          birth: { year: 2000, month: 2, day: 2 },
-          age: 11,
-          height: 160,
-          area: "area",
-          features: ["empty"],
-          price: 3000000,
-          createdAt: adminFirestore.FieldValue.serverTimestamp(),
-          updatedAt: "",
-          likeUserIDs: [theirId],
-          isAvairable: true,
-          pv: 0,
-          sendMessageUserIDs: [theirId],
-          messageUpdatedAt: "",
-          latestMessage: "ooooo",
-          clientUserID: theirId,
-          ratingCompleted: false,
-          deletedAccount: false,
+          const ref = db
+            .collection("users")
+            .doc(myId)
+            .collection("posts")
+            .doc("postID");
+
+          ref.set({
+            postID: "postID",
+            userID: myId,
+            username: "username",
+            avatar: "avatar",
+            images: ["image", "image2"],
+            title: "title",
+            postText: "postText",
+            horseName: "horseName",
+            category: "category",
+            breed: "breed",
+            gender: "gender",
+            color: "color",
+            birth: { year: 2000, month: 2, day: 2 },
+            age: 11,
+            height: 160,
+            area: "area",
+            features: ["empty"],
+            price: 3000000,
+            createdAt: firestore.FieldValue.serverTimestamp(),
+            updatedAt: "",
+            likeUserIDs: [theirId],
+            isAvairable: true,
+            pv: 0,
+            sendMessageUserIDs: [theirId],
+            messageUpdatedAt: "",
+            latestMessage: "ooooo",
+            clientUserID: theirId,
+            ratingCompleted: false,
+            deletedAccount: false,
+          });
+
+          ref.update({
+            title: "title 変更",
+          });
+
+          ref.delete();
         });
-      });
-
-      test("should be succeeded", () => {
-        const db = helper.app(projectID, auth).firestore();
-        assertSucceeds(db.doc(ref.path).get());
       });
     });
   });
+
   afterAll(async () => await helper.cleanup());
 });
