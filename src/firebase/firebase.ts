c8d4bd565d2f797582fff12584fb7f21f171e277
kotu-kotuo <ttn-0223@mineo.jp>
2021-08-21 12:25:41 +0900

    firebase/analitycs追加

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -2,6 +2,7 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 import "firebase/storage";
+import "firebase/analytics";
 import { firebaseConfig } from "./config";
 
 if (firebase.apps.length === 0) {
