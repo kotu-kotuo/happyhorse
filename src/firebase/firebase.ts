6d82f0b68139febf1f738a91ef1de754b6806c6b
kotu-kotuo <ttn-0223@mineo.jp>
2021-08-21 12:52:50 +0900

    isSupported追加

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -2,16 +2,21 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 import "firebase/storage";
-// import "firebase/analytics";
+import "firebase/analytics";
 import { firebaseConfig } from "./config";
 
 if (firebase.apps.length === 0) {
   firebase.initializeApp(firebaseConfig);
+  firebase.analytics.isSupported().then((isSupported) => {
+    if (isSupported) {
+      firebase.analytics();
+    }
+  });
 } else {
   firebase.app();
 }
 
-const analytics = firebase.analytics();
+// const analytics = firebase.analytics();
 
 export const auth = firebase.auth();
 export const db = firebase.firestore();
