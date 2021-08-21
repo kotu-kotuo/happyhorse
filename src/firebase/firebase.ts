46c2ac26bd10ac725cadb003c2c04f3f07bd02f0
kotu-kotuo <ttn-0223@mineo.jp>
2021-08-21 14:33:36 +0900

    firebase/analitycsコメントアウト

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -2,7 +2,7 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 import "firebase/storage";
-import "firebase/analytics";
+// import "firebase/analytics";
 import { firebaseConfig } from "./config";
 
 if (firebase.apps.length === 0) {
@@ -15,8 +15,6 @@ if (firebase.apps.length === 0) {
 } else {
   firebase.app();
 }
-
-// const analytics = firebase.analytics();
 
 export const auth = firebase.auth();
 export const db = firebase.firestore();
